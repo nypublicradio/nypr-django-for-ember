b2939b836f85c491fc765155366191cf96af64b4
Jeff Keen <jeff@keen.me>
2017-08-24 16:59:29 -0500

    Move some djange page stuff around to where it's supposed to be after pulling in history

---
+++ tests/unit/adapters/django-page-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('adapter:django-page', 'Unit | Adapter | django page', {
+  // Specify the other units that are required for this test.
+  // needs: ['serializer:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let adapter = this.subject();
+  assert.ok(adapter);
+});
