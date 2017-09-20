fa0b24faf2297df2d4795f986992b0520ea2342a
Brian Whitton <me@brianwhitton.com>
2017-09-20 17:03:48 -0400

    adds dummy config values

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -30,6 +30,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.webRoot = 'http://localhost:4567';
+    ENV.publisherAPI = 'http://localhost:4567/api';
   }
 
   if (environment === 'test') {
