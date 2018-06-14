88afc394754edb8324ddbe325d319dc2e7e97bf0
deploy <deploy@circleci>
2018-06-14 15:17:49 +0000

    [skip ci] Deployed d650606

--- assets/vendor-31ef73c759647f83e17307341958ef73.js
+++ assets/vendor-31ef73c759647f83e17307341958ef73.js
@@ -6168,12 +6168,10 @@ default:return"legacy"}}),didReceiveAttrs:function(){this.get("page")!==this._la
 if(n!==this._lastPage){this._lastPage=n
 var i=this.$(".django-content")
 i.empty(),(0,r.isInDom)(n.get("id"))&&(0,r.clearAlienDom)(),this.get("page").appendTo(i).then(function(){e.set("showingOverlay",!0),e.get("session.data.isStaff")&&e.revealStaffLinks(e.$(),t.default.adminRoot),e.$().imagesLoaded().progress(function(e,t){Ember.run(function(){t.img.classList.add("is-loaded")})})})}}})}),define("nypr-django-for-ember/instance-initializers/link-handler",["exports","ember-get-config"],function(e,t){"use strict"
-function n(e,t){for(;e.parentNode;){if(e.matches(t))return e
+function n(e,t){for(;e.parentNode;){if(e.matches||(e.matches=e.msMatchesSelector),e.matches(t))return e
 e=e.parentNode}}function r(e){var n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:location,r=/^https?:/i,i=e.getAttribute("href")||"",o=new URL(i,n).toString(),a=!1,s=t.default.webRoot.replace(r,""),l=o.replace(r,"")
 return i.startsWith("#")||i.startsWith("mailto:")?{url:o,href:i,isExternal:a}:(l.startsWith(s)?i=l.replace(s,"").replace(/^\//,"")||"/":i.startsWith("/")&&!i.startsWith("//")||(i="",a=!0),{url:o,href:i,isExternal:a})}function i(e){var t=r(e,arguments.length>1&&void 0!==arguments[1]?arguments[1]:location).href
-return"_blank"!==e.getAttribute("target")&&(!Array.from(e.classList).includes("ember-view")&&(!e.getAttribute("data-ember-action")&&(!(!t||t.startsWith("#")||t.startsWith("?")||t.startsWith("mailto:"))&&!(t.split(".").length>1))))}Object.defineProperty(e,"__esModule",{value:!0}),e.normalizeHref=r,e.shouldHandleLink=i,e.default={name:"link-handler",initialize:function(e){var t=e.lookup("service:wnyc-routing"),o=function(e,t,o){var a=o.target
-a.matches||(a.matches=a.msMatchesSelector)
-var s=n(a,"a")
+return"_blank"!==e.getAttribute("target")&&(!Array.from(e.classList).includes("ember-view")&&(!e.getAttribute("data-ember-action")&&(!(!t||t.startsWith("#")||t.startsWith("?")||t.startsWith("mailto:"))&&!(t.split(".").length>1))))}Object.defineProperty(e,"__esModule",{value:!0}),e.normalizeHref=r,e.shouldHandleLink=i,e.default={name:"link-handler",initialize:function(e){var t=e.lookup("service:wnyc-routing"),o=function(e,t,o){var a=o.target,s=n(a,"a")
 if(s){var l=r(s),c=l.url,u=l.href,d=l.isExternal,p=i(s)
 if(a.getAttribute("data-tracking-category")&&a.getAttribute("data-tracking-action")&&function(e,t){var n=t.lookup("service:metrics")
 if(n){var r=e.trackingCategory,i=e.trackingAction,o="",a=null
