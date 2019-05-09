f8963766b581c7d43413961bcb67e172efbdfa7d
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-05-09 16:00:35 -0400

    get wqxr djangopages from a different url

--- addon/adapters/django-page.js
+++ addon/adapters/django-page.js
@@ -21,7 +21,16 @@ export default DS.Adapter.extend({
     if (query) {
       path = `${path}?${query}`;
     }
-    return fetch(`${config.webRoot}/${path}`, { headers: {'X-WNYC-EMBER':1}})
+
+    // wqxr django pages live under a different url now that wqxr is on Fastboot
+    let url;
+    if (config.siteId == 2){
+      url = `${config.publisherAPI}/v3/wqxr_djangopages/${path}`
+    } else {
+      url = `${config.webRoot}/${path}`
+    }
+
+    return fetch(`${url}`, { headers: {'X-WNYC-EMBER': 1}})
       .then(checkStatus)
       .then(response => response.text());
     // END-SNIPPET
